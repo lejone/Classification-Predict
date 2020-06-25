b2c0f6af86a23b6d2ba05ad3ea3eacd61c129691
lejone <36841077+lejone@users.noreply.github.com>
2020-06-25 10:37:51 +0200

    draft 1

--- base_app.py
+++ base_app.py
@@ -27,6 +27,13 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
+import numpy as np
+# Graphing Libraries
+# Visualisation Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+from nltk import TreebankWordTokenizer
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -34,6 +41,26 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
+
+chart_data = pd.DataFrame(np.random.randn(50),columns=["a"])
+
+# bag of words
+def bag_of_words_count(words, word_dict={}):
+    """ this function takes in a list of words and returns a dictionary 
+        with each word as a key, and the value represents the number of 
+        times that word appeared"""
+    for word in words:
+        if word in word_dict.keys():
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+    return word_dict
+
+# tokenise data
+tokeniser = TreebankWordTokenizer()
+
+word_dict = {}
+
 
 # The main function where we will build the actual app
 def main():
@@ -46,7 +73,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information", "Graphics"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
@@ -64,6 +91,8 @@ def main():
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
+		tokens = tokeniser.tokenize(tweet_text)
+		word_dict = bag_of_words_count(tokens)
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
@@ -77,6 +106,37 @@ def main():
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+			word_df = pd.DataFrame(word_dict.items(), index = word_dict.keys())
+			#word_df.set_index(0)
+			st.bar_chart(word_df)
+
+	# Building out the "Graphics" page
+	if selection == "Graphics":
+		# You can read a markdown file from supporting resources folder
+		#st.markdown("Most Frequently used words")
+
+		st.subheader("Detailed Information about the Tweet")
+		#graph = st.radio(
+		#"What Information Do You Want To View?",
+		#('Number of Words Used', 'Frequently used Words', 'More..'))
+
+		#if graph == 'Word Cloud':
+			#wordcloud_ = WordCloud(width=1500, height = 900).generate(raw['message'][0])
+			# Display the generated image:
+			#plt.imshow(wordcloud_, interpolation='bilinear')
+			#plt.set_title('Words in Tweet')
+			#plt.axis('off')
+			#st.pyplot()
+            # Show the ditribution of the classes as a graph
+		#elif graph == 'Word Count':
+			#f, ax = plt.subplots(figsize=(10, 8))
+			#sns.set(style="whitegrid")
+			#ax = sns.countplot(x="message", data=raw['message'][0])
+			#plt.title('Message Count', fontsize =20)
+			#st.pyplot()
+            
+		#elif graph == 'Number of Words Used':
+		st.bar_chart(word_dict)
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
